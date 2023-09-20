3a8e5cd497ecf9fd814caf3954fbfd34712c58a0
Enrique <someemail@gmail.com>
2023-09-20 17:14:33 -0400

    Initial Commit

---
+++ GitProject/GitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitProjectApp.swift
+//  GitProject
+//
+//  Created by Loaner on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct GitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
